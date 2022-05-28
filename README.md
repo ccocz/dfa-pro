6ea5bd20dbfb19fb37b6936c028e9f800b55159f
Resul Hangeldiyev <rh402185@students.mimuw.edu.pl>
2022-05-28 12:21:24 +0200

    init
    * src and docs folders

---
+++ README.md
@@ -0,0 +1,1 @@
+# Automata implementation in Prolog
---
+++ src/rh402185.pl
@@ -0,0 +1,7 @@
+% @author Resul Hangeldiyev
+
+% correct(+Automat, -Reprezentacja)
+% accept(+Automat, ?Słowo)
+% empty(+Automat)
+% equal(+Automat1, +Automat2)
+% subsetEq(+Automat1, +Automat2)
