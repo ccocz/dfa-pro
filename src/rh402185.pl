7a0a4a16a2d4d6066b739a3ba99ce3c197b3bc43
Resul Hangeldiyev <rh402185@students.mimuw.edu.pl>
2022-05-30 15:53:19 +0200

    Add checks for DFA validity
    * Combine checks

--- src/rh402185.pl
+++ src/rh402185.pl
@@ -1,5 +1,10 @@
 % @author Resul Hangeldiyev
 
+/*
+ * todos:
+ * - check if x-y pair representation is valid
+ */
+
 % Binary search tree with key/value pair
 % bst(empty).
 % bst(node(L, _, P)) :- bst(L), bst(P).
@@ -30,6 +35,12 @@ getKeyBST(node(_, K-_, R), K0, V1) :-
     K0 @> K,
     getKeyBST(R, K0, V1).
 
+% allPresent(List, BST) true iff all keys from list appears in BST.
+allPresent([], _).
+allPresent([L | R], T) :-
+    getKeyBST(T, L, _),
+    allPresent(R, T).
+
 % dfa(TransitionFunction, StartingState, FinalStates)
 
 transitionFunction([]).
@@ -38,7 +49,7 @@ transitionFunction([fp(_, _, _) | R]) :- transitionFunction(R).
 dfa(TF, _, _) :- transitionFunction(TF).
 
 % Get all states out of transitions, stored in BST.
-getStates([], T) :- getStates([], empty, T).
+getStates(L, T) :- getStates(L, empty, T).
 getStates([], T, T).
 getStates([fp(S1, _, S2) | Y], A, T) :-
     insertBST(A, S1-1, A0),
@@ -46,16 +57,17 @@ getStates([fp(S1, _, S2) | Y], A, T) :-
     getStates(Y, A1, T).
 
 % Get alphabet out of transitions, stored in BST.
-getAlphabet([], T) :- getAlphabet([], empty, T).
+getAlphabet(L, T) :- getAlphabet(L, empty, T).
 getAlphabet([], T, T).
 getAlphabet([fp(_, C, _) | Y], A, T) :-
     insertBST(A, C-1, A0),
     getAlphabet(Y, A0, T).
 
 % Get transitions as BST.
-getTransitions([], T) :- getTransitions([], empty, T).
+getTransitions(L, T) :- getTransitions(L, empty, T).
 getTransitions([], T, T).
 getTransitions([fp(S1, C, S2) | Y], A, T) :-
+    \+ getKeyBST(A, (S1-C), _),
     insertBST(A, (S1-C)-S2, A0),
     getTransitions(Y, A0, T).
 
@@ -65,10 +77,39 @@ doAllExist([X | Y], T) :-
     getKeyBST(T, X, _),
     doAllExist(Y, T).
 
-% DFA(states, alphabet, transition, start, final)
+% cartesianProduct(BST_X, BST_Y, List) is true iff 
+% [keys of BST_X] * [keys of BST_Y] = List.
+cartesianProduct(X, Y, Z) :- cartesianProduct(X, Y, [], Z).
+cartesianProduct(empty, _, A, A).
+cartesianProduct(node(L, K-_, R), Y, A, Z) :-
+    prependAll(K, Y, A, LY),
+    cartesianProduct(L, Y, LY, Z0),
+    cartesianProduct(R, Y, Z0, Z).
+
+% Create pairs of given element with all nodes of given BST.
+prependAll(X, Y, Z) :- prependAll(X, Y, [], Z).
+prependAll(_, empty, A, A).
+prependAll(X, node(L, K-_, R), A, Z) :-
+    prependAll(X, L, [(X-K) | A], Z0),
+    prependAll(X, R, Z0, Z).
+
+
+% checkFullGraph(States, Alphabet, Transitions) true iff single outgoing
+% edge for each letter in alphabet.
+checkFullGraph(S, A, T) :-
+    cartesianProduct(S, A, P),
+    allPresent(P, T).
+
+% dfaInternal(states, alphabet, transitions, start, final)
 
 % correct(+Automat, -Reprezentacja)
-correct(dfa(TF, SS, FS), dfa(TF, SS, FS)).
+correct(dfa(TF, SS, FS), dfaInternal(S, A, T, SS, FS)) :-
+    getTransitions(TF, T),
+    getStates(TF, S),
+    getAlphabet(TF, A),
+    checkFullGraph(S, A, T),
+    getKeyBST(S, SS, _),
+    allPresent(FS, S).
 
 % accept(+Automat, ?Słowo)
 accept(dfa(_, SS, FS), []) :- member(SS, FS).
